e872607eede4c96ad686bc9a3af1c1a2c9670120
EllenPinheiro <ellen.pinheiro2@estudante.ifms.edu.br>
2023-09-21 14:11:15 -0300

    salvando novo código

--- algoritmo.js
+++ algoritmo.js
@@ -2,173 +2,68 @@ import fs from 'fs';          // Módulo 'fs' para lidar com operações de arqu
 import csv from 'csv-parser'; // Módulo 'csv-parser' para analisar arquivos CSV
 
 
-// Constantes para as planilhas
-const projetosNotasFile = 'projetos-notas.csv';
-const criteriosPesosFile = 'criterios-pesos.csv';
+// Arquivos CSV de entrada
+const projetosNotasCSV = 'projetos-notas.csv';
+const criteriosPesosCSV = 'criterios-pesos.csv';
 
+// Função para ler o arquivo CSV de critérios e pesos
+const lerCSVCriteriosPesos = () => {
+  let criteriosPesos = {}; // Objeto para armazenar critérios e seus pesos
+  // Lê o arquivo CSV linha por linha e adiciona os dados ao objeto criteriosPesos
+  fs.createReadStream(criteriosPesosCSV)
+    .pipe(csv()) // Usa o csv-parser
+    .on('data', (row) => {
+      const [criterio, peso] = row;
+      criteriosPesos[criterio] = parseFloat(peso); // Converte o peso de string para número e associa ao critério correspondente
+    })
+    .on('end', () => {
+      console.log('Critérios e pesos:', criteriosPesos); // Exibe os critérios e pesos após a leitura do CSV
+      lerCSVProjetosNotas(); // Chama a função para ler o arquivo de projetos e notas após ler os critérios e pesos
+    });
+};
 
-// Arrays para armazenar os dados
-const projetosNotas = [];
-const criteriosPesos = [];
-const mediasPonderadas = [];
+// Função para ler o arquivo CSV de projetos e notas
+const lerCSVProjetosNotas = () => {
+  const projetosNotas = []; // Array para armazenar os dados de projetos e notas
 
-// Stream de leitura para "projetos-notas.csv"
-const readableStreamProjetosNotas = fs.createReadStream(projetosNotasFile);
-const transformToObject = csv({ separator: ';' }); // Transforma em objeto
+  // Lê o arquivo CSV linha por linha e adiciona os dados ao array projetosNotas
+  fs.createReadStream(projetosNotasCSV)
+    .pipe(csv()) // Usa o csv-parser para analisar o CSV
+    .on('data', (row) => {
+      projetosNotas.push(row); // Adiciona a linha atual ao array projetosNotas
+    })
+    .on('end', () => {
+      // Remove a primeira linha (cabeçalho) que contém as nomeações "n1" até "n7"
+      projetosNotas.shift();
+      calcularMediasPonderadas(projetosNotas); // Quando a leitura do CSV terminar, chama a função para calcular as médias ponderadas
+    });
+};
 
+// Função para calcular as médias ponderadas dos projetos
+const calcularMediasPonderadas = (projetosNotas) => {
+  const mediasPonderadas = []; // Array para armazenar as médias ponderadas dos projetos
 
-// Stream de transformação para calcular as médias ponderadas
-const calcularMediasPonderadas = new Transform({
-  objectMode: true,
-  transform(chunk, encoding, callback) {
-    const notas = [];
-    for (let i = 4; i <= 10; i++) {
-      notas.push(Number(chunk[`n${i}`]));
+  projetosNotas.forEach((projeto) => {
+    let somaNotasPonderadas = 0; // Variável para somar as notas ponderadas de um projeto
+
+    // Loop para iterar pelas colunas de notas da 5° à 11° (n1 a n7)
+    for (let i = 5; i <= 11; i++) {
+      const nota = parseFloat(projeto[`n${i}`]); // Obtém a nota da coluna atual e converte para número
+      const peso = criteriosPesos[i.toString()];   // Obtém o peso correspondente ao critério atual
+      somaNotasPonderadas += nota * peso; // Calcula a nota ponderada e adiciona à soma
     }
 
-
-    // Verifique se "fk_projeto" existe antes de tentar convertê-lo
-    const projetoId = chunk.hasOwnProperty('fk_projeto') ? Number(chunk.fk_projeto) : null;
-
-
-    if (projetoId !== null && !isNaN(projetoId)) {
-      // Encontre os pesos corretos para este projeto
-      const criterioPeso = criteriosPesos.find((item) => item.criterio === projetoId);
-
-
-      if (criterioPeso) {
-        const pesos = criterioPeso.pesos.split(',').map(Number);
-
-
-        const mediaPonderada = notas.reduce((acc, nota, index) => {
-          return acc + nota * pesos[index];
-        }, 0);
-
-
-        chunk.mediaPonderada = mediaPonderada;
-        mediasPonderadas.push({ projetoId, mediaPonderada });
-      } else {
-        console.log('Projeto ID não encontrado em criterios-pesos.csv:', projetoId);
-      }
-    } else {
-      console.log('Projeto ID inválido:', chunk.fk_projeto);
-    }
-
-
-    callback(null, chunk);
-  },
-});
-
-
-// Stream de saída para exibir as médias ponderadas
-const writableStreamMediasPonderadas = new Writable({
-  objectMode: true,
-  write(chunk, encoding, next) {
-    console.log(`Projeto ID: ${chunk.projetoId}, Média Ponderada: ${chunk.mediaPonderada}`);
-    next();
-  },
-});
-
-
-// Stream de leitura para "criterios-peso.csv"
-const readableStreamCriteriosPesos = fs.createReadStream(criteriosPesosFile);
-const transformCriteriosPesosToObject = csv({ separator: ',' }); // Alterado para ','
-
-
-// Stream de saída para armazenar critérios e pesos
-const writableStreamCriteriosPesos = new Writable({
-  objectMode: true,
-  write(chunk, encoding, next) {
-    criteriosPesos.push(chunk);
-    next();
-  },
-});
-
-
-// Execute o processo de leitura de critérios-peso.csv
-readableStreamCriteriosPesos
-  .pipe(transformCriteriosPesosToObject)
-  .pipe(writableStreamCriteriosPesos)
-  .on('finish', () => {
-    // Quando a leitura de "criterios-peso.csv" for concluída,
-    // inicie o processamento de "projetos-notas.csv"
-
-
-    readableStreamProjetosNotas
-      .pipe(transformToObject)
-      .pipe(calcularMediasPonderadas)
-      .pipe(writableStreamMediasPonderadas)
-      .on('finish', () => {
-        // Após calcular as médias ponderadas, continue com o Método de Copeland e ranqueamento.
-
-
-        // Função para calcular a matriz de decisão do Método de Copeland
-        function calcularMatrizDecisao(mediasPonderadas) {
-          const numProjetos = mediasPonderadas.length;
-          const matrizDecisao = Array.from({ length: numProjetos }, () => Array(numProjetos).fill(0));
-
-
-          for (let i = 0; i < numProjetos; i++) {
-            for (let j = i + 1; j < numProjetos; j++) {
-              if (mediasPonderadas[i].mediaPonderada > mediasPonderadas[j].mediaPonderada) {
-                matrizDecisao[i][j] = 1;
-                matrizDecisao[j][i] = -1;
-              } else if (mediasPonderadas[i].mediaPonderada < mediasPonderadas[j].mediaPonderada) {
-                matrizDecisao[i][j] = -1;
-                matrizDecisao[j][i] = 1;
-              }
-            }
-          }
-
-
-          return matrizDecisao;
-        }
-
-
-        // Calcular a matriz de decisão do Método de Copeland
-        const matrizDecisao = calcularMatrizDecisao(mediasPonderadas);
-
-
-        // Função para calcular as somas das linhas da matriz de decisão
-        function calcularSomasLinhas(matrizDecisao) {
-          const numProjetos = matrizDecisao.length;
-          const somasLinhas = Array(numProjetos).fill(0);
-
-
-          for (let i = 0; i < numProjetos; i++) {
-            for (let j = 0; j < numProjetos; j++) {
-              somasLinhas[i] += matrizDecisao[i][j];
-            }
-          }
-
-
-          return somasLinhas;
-        }
-
-
-        // Calcular as somas das linhas da matriz de decisão
-        const somasLinhas = calcularSomasLinhas(matrizDecisao);
-
-
-        // Gerar o ranqueamento geral
-        const ranqueamentoGeral = mediasPonderadas.map((projeto, index) => ({
-          projetoId: projeto.projetoId,
-          mediaPonderada: projeto.mediaPonderada,
-          somaLinhas: somasLinhas[index],
-        }));
-
-
-        // Ordenar o ranqueamento geral pelo critério desejado (por exemplo, somaLinhas)
-        ranqueamentoGeral.sort((a, b) => b.somaLinhas - a.somaLinhas);
-
-
-        // Exibir o ranqueamento geral
-        console.log('Ranqueamento Geral:');
-        ranqueamentoGeral.forEach((projeto, index) => {
-          console.log(`${index + 1}. Projeto ID: ${projeto.projetoId}, Soma das Linhas: ${projeto.somaLinhas}`);
-        });
-
-
-        console.log('Processamento concluído.');
-      });
+    mediasPonderadas.push(somaNotasPonderadas); // Adiciona a média ponderada do projeto ao array
   });
+
+  console.log('Médias ponderadas dos projetos:', mediasPonderadas); // Exibe as médias ponderadas no console
+};
+
+// Função principal para executar o programa
+const main = () => {
+  // Chama a função para ler o arquivo CSV de critérios e pesos
+  lerCSVCriteriosPesos();
+};
+
+// Chamada da função principal para iniciar o programa
+main();
